c9d8ba64dd8912ba358f72b46f1954af826f35d2
GoddessD <dlouis314@gmail.com>
2022-10-27 23:23:35 +0000

    update

--- src/app.js
+++ src/app.js
@@ -6,22 +6,6 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  
-};
-
-let getRandom = limit => {
-  return Math.floor(Math.random() * limit);
-  
-  let generateCard = () => {
-    let suits = ["hearts", "spades", "diamonds", "clubs"];
+  let suits = ["hearts", "spades", "diamonds", "clubs"];
   let ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
-
-
-return (
-  suits[getRandom(suits.length)] +
-  " " +
-  ranks[getRandom(ranks.length)] +
-  " " +
-);
 };
-document.querySelector("#.card").innerHTML = generateCard();
