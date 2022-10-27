539523f745a048ad50346ed991774773e0807af6
GoddessD <dlouis314@gmail.com>
2022-10-27 23:17:38 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  
 };
+
+let getRandom = limit => {
+  return Math.floor(Math.random() * limit);
+  
+  let generateCard = () => {
+    let suits = ["hearts", "spades", "diamonds", "clubs"];
+  let ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
+
+return (
+  suits[getRandom(suits.length)] +
+  " " +
+  ranks[getRandom(ranks.length)] +
+  " " +
+);
+};
+document.querySelector("#.card").innerHTML = generateCard();
