db86feac50192926152096d61c0b7ad0189204bd
GoddessD <dlouis314@gmail.com>
2022-10-28 21:25:33 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -8,4 +8,13 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   let suits = ["hearts", "spades", "diamonds", "clubs"];
   let ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let card = document.querySelector(".card");
+  let cardRank = document.querySelector(".number");
+
+  let getRandom = limit => {
+    return Math.floor(Math.random() * limit);
+  };
+
+  card.classList.add(suits[getRandom(suits.length)]);
+  cardRank.innerHTML = ranks[getRandom(ranks.length)];
 };
